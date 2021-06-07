436d02984200b75f7af5219036ff5849981122ab
Aledor <mohdfitri93@hmail.com>
2021-06-07 16:32:14 +0800

    newLablife

---
+++ New Unity Project/Assets/GetBacteria.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetBacteria : MonoBehaviour
+{
+
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "Pickupable")
+        {
+            BacteriaSript bacteria = GameObject.Find("Swap").GetComponent<BacteriaSript>();
+            gameObject.GetComponent<BacteriaSript>();
+
+            if (bacteria.bacteriaison == false)
+            {
+                Debug.Log("GotIt");
+
+            }
+        }
+    }
+}
+
+   
