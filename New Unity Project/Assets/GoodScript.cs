7b7553365847f06519c5e3751e7e67c46cc78e9c
Aledor <mohdfitri93@hmail.com>
2021-06-08 04:02:03 +0800

    Almost there

---
+++ New Unity Project/Assets/GoodScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodScript : MonoBehaviour
+{
+    public bool isGood = true;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
