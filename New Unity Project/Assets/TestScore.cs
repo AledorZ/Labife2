fa78496ad74d9f96a750a4390e6468916ae6500e
Aledor <mohdfitri93@hmail.com>
2021-06-09 00:45:49 +0800

    Incubator tutorial done

--- New Unity Project/Assets/TestScore.cs
+++ New Unity Project/Assets/TestScore.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class TestScore : MonoBehaviour
 {
+    public float TimerToWin = 0.0f;
+    [SerializeField] bool Agarisin = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,15 +16,31 @@ public class TestScore : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Agarisin == true)
+        {
+            Timer();
+        }
+
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Pickupable"))
         {
-            ScoreScritp.ScoreCOP += 1;
-            ScoreScritp.ScoreAML -= 1;
+            Agarisin = true;
+            Debug.Log("AgarIsin");
+           // ScoreScritp.ScoreCOP += 1;
+           // ScoreScritp.ScoreAML -= 1;
             //ScoreScritp.ScoreCOT += 1;
         }
     }
+    public void Timer()
+    {
+        TimerToWin += Time.deltaTime;
+        if(TimerToWin >= 8.0f)
+        {
+            ScoreScritp.ScoreCOT = +1;
+        }
+        
+    }
 }
+
